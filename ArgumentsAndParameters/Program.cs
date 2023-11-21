6a0f5fd299798cf379990cade13e4467c03713dc
Valeria <valerija.kozlova@tktk.ee>
2023-11-21 11:30:21 +0200

    week7 functions

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); //userInput - argument
+
+
+
+static void PrintAnyWord(string anyString) //anyString - parameter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
